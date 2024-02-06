b82aa1a1b97866cf89c94a96f2a3c3bfe471d2da
HAMA Barhamou <hamabarhamou@gmail.com>
2024-02-06 12:13:46 +0100

    Ajout des étiquettes affichant les compteurs d'étapes totales, terminées et en retard.

--- src/main/java/com/company/onboarding/view/myonboarding/MyOnboardingView.java
+++ src/main/java/com/company/onboarding/view/myonboarding/MyOnboardingView.java
@@ -6,12 +6,15 @@ import com.company.onboarding.entity.UserStep;
 import com.company.onboarding.view.main.MainView;
 
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.CollectionLoader;
+import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -28,12 +31,21 @@ public class MyOnboardingView extends StandardView {
 
     @ViewComponent
     private CollectionLoader<UserStep> userStepsDl;
+    @ViewComponent
+    private Span completedStepsLabel;
+    @ViewComponent
+    private  Span overdueStepsLabel;
+    @ViewComponent
+    private Span totalStepsLabel;
+    @ViewComponent
+    private CollectionContainer<UserStep> userStepsDc;
 
     @Subscribe
     public void onBeforeShow(final BeforeShowEvent event) {
         final User user = (User) currentAuthentication.getUser();
         userStepsDl.setParameter("user", user);
         userStepsDl.load();
+        updateLabels();
     }
 
     @Supply(to = "userStepsDataGrid.completed", subject = "renderer")
@@ -54,5 +66,30 @@ public class MyOnboardingView extends StandardView {
             });
             return checkbox;
         });
+    }
+
+    private void updateLabels() {
+        totalStepsLabel.setText("Total steps: " + userStepsDc.getItems().size());
+
+        long completedCount = userStepsDc.getItems().stream()
+                .filter(us -> us.getCompletedDate() != null)
+                .count();
+        completedStepsLabel.setText("Completed steps: " + completedCount);
+
+        long overdueCount = userStepsDc.getItems().stream()
+                .filter(this::isOverdue)
+                .count();
+        overdueStepsLabel.setText("Overdue steps: " + overdueCount);
+    }
+
+    private boolean isOverdue(UserStep us) {
+        return us.getCompletedDate() == null
+                && us.getDueDate() != null
+                && us.getDueDate().isBefore(LocalDate.now());
+    }
+
+    @Subscribe(id = "userStepsDc", target = Target.DATA_CONTAINER)
+    public void onUserStepsDcItemPropertyChange(final InstanceContainer.ItemPropertyChangeEvent<UserStep> event) {
+        updateLabels();
     }
 }
